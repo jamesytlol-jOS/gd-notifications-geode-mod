d84e1ef196f23ffd2079f486857b0278edd8e1d4
jamesytlol-jOS <jamesytlolgminor@gmail.com>
2026-01-01 14:22:04 +0000

    Simplify for efficiency
    Simplified the cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,15 @@
 
 using namespace geode::prelude;
 
-// We are telling Geode to "modify" the MenuLayer (Main Menu)
 class $modify(MenuLayer) {
     bool init() {
-        // Run the original game's MenuLayer::init() first
-        // If the game fails to load the menu, we stop here
         if (!MenuLayer::init()) {
             return false;
         }
 
-        // Create and show a Geode notification
-        // Arguments: "Text", Icon Type
-        Notification::create(
-            "Hello from my first mod!", 
-            NotificationIcon::Success
-        )->show();
+        // Use a simpler notification method to avoid the 'fmt' bug
+        auto notification = Notification::create("Mod Loaded!", NotificationIcon::Success);
+        notification->show();
 
         return true;
     }
